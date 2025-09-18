6d526d608fdd54816347561a277e9fa2266858a5
Chris "Slothy <69122728+Im2Slothy@users.noreply.github.com>
2025-09-18 13:40:36 -0500

    Add initial structure and TODOs for study assistant

---
+++ main.py
@@ -0,0 +1,21 @@
+# TODO
+
+'''
+Welcome the user, find someway for the user to give the topic or photos/pdfs of what they're working with if they choose. 
+Ask the user for a specific study method or a simple timer. 
+Maybe a config for different study methods? ( This does this and that - Have them enter their choice via numbers or text ) - use.lower
+
+When we're ready to begin, ensure we have camera access - Document in comments or somewhere to help the user solve the errors.
+
+When we're started, say X photos a second or every few seconds -- We'll compare what we see with the trained model --
+If user is caught using phone
+  Work with arduino to play buzzer -- Are we doing something with the time?
+else:
+  Continue and play a task or loop to take photos and check.
+
+While this is happening we either play the timer on the screen or just print every minute in the console. 
+
+When we hit milestones (Specify in the config -- Tell the user they're doing)
+
+Integrate ChatGPT into the code for checks of assignment and stuff - run this last. 
+'''
